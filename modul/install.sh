68eca1fb20632fbd64aba6334ee3b3ca3b913dfd
Bl4ckDr460n <49341995+Bl4ckDr460n@users.noreply.github.com>
2019-04-07 04:53:22 +0700

    Add files via upload

---
+++ modul/install.sh
@@ -0,0 +1,20 @@
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install cowsay
+pkg install figlet
+pkg install ruby
+pkg install git
+pkg install cmatrix
+pkg install sl
+clear
+echo $ku
+figlet -f slant.flf "Done"
