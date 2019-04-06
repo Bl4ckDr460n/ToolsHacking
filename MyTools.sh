d04cd2d6c05e4914bff42ba94686d259de9c5438
Bl4ckDr460n <49341995+Bl4ckDr460n@users.noreply.github.com>
2019-04-07 04:47:29 +0700

    Add files via upload

---
+++ MyTools.sh
@@ -0,0 +1,31 @@
+##########################################################
+# Author     : Billal { Bl4ck_Dr460n			 #
+# Name Tools : MyTools					 #
+# Version    : 1.0					 #
+# Code       : rooted@dr460n				 #
+##########################################################
+# [!] Gunakan Dengan Bijak Jangan SalahKan Tools Ini [!] #
+##########################################################
+#                        MyTeam				 #
+##########################################################
+#                   Black Cyber Army			 #
+#              Indonesia Attacker Public   		 #
+#                   Subang Cyber Team			 #
+##########################################################
+biru='\033[34;1m' #biru
+daun='\033[32;1m' #ijo
+purple='\033[35;1m' #purple
+cyyan='\033[36;1m' #cyan
+merah='\033[31;1m' #merah
+putih='\033[37;1m' #pur
+tai='\033[33;1m' #kuning
+clear
+echo $daun
+echo $daun "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo $duan " %        "$tai   "INFO             "$daun"  %"
+echo $daun "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo $daun "%"$merah" Author 	 = Billal     "$daun"%"
+echo $daun "%"$merah" Name Tools = MyTools       "$daun"%"
+echo $daun "%"$merah" Version	 = 1.0        "$daun"%"
+echo $daun "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+figlet "C o o m i n g  S o o n"
