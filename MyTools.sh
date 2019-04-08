deaf7adc6395b34c30dcfc2dbdac4dfa4ae1c062
Bl4ck_Dr460n <49341995+Bl4ckDr460n@users.noreply.github.com>
2019-04-08 19:14:14 +0700

    Add files via upload

--- MyTools.sh
+++ MyTools.sh
@@ -22,19 +22,48 @@ tai='\033[33;1m' #kuning
 clear
 echo $daun
 echo $daun "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+sleep 0.9
 echo $duan " %        "$tai   "INFO             "$daun"  %"
+sleep 0.9
 echo $daun "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+sleep 0.9
 echo $daun "%"$merah" Author 	 = Billal     "$daun"%"
-echo $daun "%"$merah" Name Tools = MyTools       "$daun"%"
+sleep 0.9
+echo $daun "%"$merah" Name Tools    = MyTools    "$daun"%"
+sleep 0.9
 echo $daun "%"$merah" Version	 = 1.0        "$daun"%"
+sleep 0.9
+echo $daun "%"$merah" Umur       = 13 Tahun      "$daun"%"
 echo $daun "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 	echo
 	echo
+			echo $biru "[==========================]"
+			echo $biru "["$tai"   GUNAKAN DENGAN BIJAK  "  $biru"]"
+			echo $biru "[==========================]"
+	echo		
 	echo
 echo $merah "[]=======================[]"
+sleep 1
 echo $daun  "  1.Hack Facebook Target"
 echo $merah "[]=======================[]"
+sleep 1
+echo $daun  "  2.Install Metasploit"
+echo $merah "[]=======================[]"
+sleep 1
+echo $daun  "  3.Tools DDOS"
+echo $merah "[]=======================[]"
+sleep 1
+echo $daun  "  4.SpamCall"
+echo $merah "[]=======================[]"
+sleep 1
+echo $daun  "  5.KKKTP Generate"
+echo $merah "[]=======================[]"
+sleep 1
+echo $daun  "  E.Exit"
+echo $merah "[]=======================[]"
+sleep 1
 echo $tai
+sleep 0.9
 read -p "dr460n >>" pilih
 
 if [ $pilih = "1" ] || [ $pilih = "1" ]
@@ -47,4 +76,52 @@ git clone https://github.com/Bl4ckDr460n/facebookbrute
 mv facebookbrute $HOME
 cd $HOME/facebookbrute
 python2 brute.py
+fi
+if [ $pilih = "2" ] || [ $pilih = "2" ]
+then
+clear
+git clone https://github.com/4L13199/meTAInstall
+mv meTAInstall $HOME
+cd $HOME/meTAInstall
+sh meTAInstall.sh
+fi
+if [ $pilih = "3" ] || [ $pilih = "3" ]
+then
+clear
+pkg install python
+pkg install python2
+git clone https://github.com/Bl4ckDr460n/DDOS
+mv DDOS $HOME
+clear
+echo $merah " Tinggal Ketik "$biru"cd $HOME/DDOS"
+echo $merah "Truss Ketik "$tai"python3 DDOS.py -s IP target -p 80 -t 135"
+sleep 3
+fi
+if [ $pilih = "4" ] || [ $pilih = "2" ]
+then
+clear
+pkg install cowsay
+git clone https://github.com/Bl4ckDr460n/spamcall
+mv spamcall $HOME
+cd $HOME/spamcall
+php Call.php
+exit
+fi
+if [ $pilih = "5" ] || [ $pilih = "5" ]
+then
+clear
+pkg install php
+git clone https://github.com/Bl4ckD460n/freekkktp
+mv freekkktp $HOME
+cd $HOME/freekkktp
+php ktp.php
+exit
+fi
+# Exit
+if [ $pilih = "E" ]
+then
+clear
+echo $tai "Ya Udah Kalo Gak Mau Ngegunain Tools Ini"
+echo $merah "Pelit Amat Sih Jadi Orang :)"
+exit
 fi
